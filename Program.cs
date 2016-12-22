4c086cc0722bcfbe72229816fee1b59c2f7ed7dd
Thanh <phamhathanh130394@gmail.com>
2016-12-22 13:00:46 +0700

    Switch to C#.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scheme
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var tokens = tokenize('((1 "2") 3 4)');
-console.log(tokens);
-var data = read(tokens);
-console.log(data);
-console.log(data.toString());
