2cb52104535750afceac2ecd80d8f536a07d03aa
ThanhPH <phamhathanh130394@gmail.com>
2016-12-23 02:13:40 +0700

    List representation.

--- src/Storage/ConsCell.cs
+++ src/Storage/ConsCell.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace Scheme.Storage
 {
@@ -15,6 +16,21 @@ namespace Scheme.Storage
         }
 
         public override string ToString()
-            => $"({Car} . {Cdr})";
+        {
+            var output = new StringBuilder($"({Car}");
+            var current = Cdr;
+            while (current is ConsCell)
+            {
+                var cc = (ConsCell)current;
+                output.Append($" {cc.Car}");
+                current = cc.Cdr;
+            }
+            if (current is Nil)
+                output.Append(')');
+            else
+                output.Append($" . {current})");
+            return output.ToString();
+            // TODO: Circular list case.
+        }
     }
 }
